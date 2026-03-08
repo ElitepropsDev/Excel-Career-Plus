7a15a0d9b957d3013770e6e013dc5dbc514a9115
Tope Bello <Belltop12@gmail.com>
2026-03-08 18:21:56 +0100

    new endpoint added

--- Frontend/src/components/LandingPage/RegistrationForm.jsx
+++ Frontend/src/components/LandingPage/RegistrationForm.jsx
@@ -36,7 +36,7 @@ const RegistrationForm = () => {
 
     try {
       // NEW: Send data to your Local Backend (Port 5000)
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch('https://excel-career-plus-backend.vercel.app/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
